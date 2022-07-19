e5c372b84849b2b03a72036e08f0e63a8de08f35
christabeladhiambo <99637155+christabeladhiambo@users.noreply.github.com>
2022-07-19 07:36:13 -0700

    Update 1-insertion_sort_list.c

--- adhis_sorted/1-insertion_sort_list.c
+++ adhis_sorted/1-insertion_sort_list.c
@@ -1,117 +1,58 @@
-nclude "sort.h"
-
-
+#include "sort.h"
 
 /**
-
-  * swap - Swap node of a double linked list
-
-  *
-
-  * @current: The current node
-
-  * @prev: The previous node
-
-  * @list: The pointer to the linked list
-
-  *
-
-  * Return: Anything, cause void function
-
-  */
-
+* swap - Swap node of a double linked list
+*
+* @current: The current node
+* @prev: The previous node
+* @list: The pointer to the linked list
+*
+* Return: Anything, cause void function
+*/
 void swap(listint_t *current, listint_t *prev, listint_t **list)
-
 {
+	listint_t *tmp;
 
-		listint_t *tmp;
+	tmp = prev->prev;
+	if (tmp)
+		tmp->next = current;
+	else
+		*list = current;
 
+	current->prev = tmp;
 
-
-			tmp = prev->prev;
-
-				if (tmp)
-
-							tmp->next = current;
-
-					else
-
-								*list = current;
-
-
-
-						current->prev = tmp;
-
-
-
-							prev->prev = current;
-
-								prev->next = current->next;
-
-									current->next = prev;
-
-										if (prev->next)
-
-													prev->next->prev = prev;
-
-											else
-
-														prev->next = NULL;
-
+	prev->prev = current;
+	prev->next = current->next;
+	current->next = prev;
+	if (prev->next)
+		prev->next->prev = prev;
+	else
+		prev->next = NULL;
 }
-
-
 
 /**
-
-  * insertion_sort_list - Sort a double linked list by comparing each
-
-  * elem with the previous
-
-  *
-
-  * @list: The linked list to sort
-
-  *
-
-  * Return: Anything, cause void function
-
-  */
-
+* insertion_sort_list - Sort a double linked list by comparing each
+* elem with the previous
+*
+* @list: The linked list to sort
+*
+* Return: Anything, cause void function
+*/
 void insertion_sort_list(listint_t **list)
-
 {
+	listint_t *browse = NULL;
 
-		listint_t *browse = NULL;
+	if (!list || !*list)
+		return;
 
-
-
-			if (!list || !*list)
-
-						return;
-
-
-
-				browse = (*list)->next;
-
-					while (browse)
-
-							{
-
-										while (browse->prev && browse->prev->n > browse->n)
-
-													{
-
-																	swap(browse, browse->prev, list);
-
-																				print_list(*list);
-
-																						}
-
-												browse = browse->next;
-
-													}
-
+	browse = (*list)->next;
+	while (browse)
+	{
+		while (browse->prev && browse->prev->n > browse->n)
+		{
+			swap(browse, browse->prev, list);
+			print_list(*list);
+		}
+		browse = browse->next;
+	}
 }
-
-
