40a341a6362893308c7e883da955af2fc05a037f
christabel <christabeladhiambo312gmail.com>
2022-07-19 07:33:21 -0700

    Insert sorted

---
+++ adhis_sorted/1-insertion_sort_list.c
@@ -0,0 +1,117 @@
+nclude "sort.h"
+
+
+
+/**
+
+  * swap - Swap node of a double linked list
+
+  *
+
+  * @current: The current node
+
+  * @prev: The previous node
+
+  * @list: The pointer to the linked list
+
+  *
+
+  * Return: Anything, cause void function
+
+  */
+
+void swap(listint_t *current, listint_t *prev, listint_t **list)
+
+{
+
+		listint_t *tmp;
+
+
+
+			tmp = prev->prev;
+
+				if (tmp)
+
+							tmp->next = current;
+
+					else
+
+								*list = current;
+
+
+
+						current->prev = tmp;
+
+
+
+							prev->prev = current;
+
+								prev->next = current->next;
+
+									current->next = prev;
+
+										if (prev->next)
+
+													prev->next->prev = prev;
+
+											else
+
+														prev->next = NULL;
+
+}
+
+
+
+/**
+
+  * insertion_sort_list - Sort a double linked list by comparing each
+
+  * elem with the previous
+
+  *
+
+  * @list: The linked list to sort
+
+  *
+
+  * Return: Anything, cause void function
+
+  */
+
+void insertion_sort_list(listint_t **list)
+
+{
+
+		listint_t *browse = NULL;
+
+
+
+			if (!list || !*list)
+
+						return;
+
+
+
+				browse = (*list)->next;
+
+					while (browse)
+
+							{
+
+										while (browse->prev && browse->prev->n > browse->n)
+
+													{
+
+																	swap(browse, browse->prev, list);
+
+																				print_list(*list);
+
+																						}
+
+												browse = browse->next;
+
+													}
+
+}
+
+
